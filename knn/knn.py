4fe389959e0bc1f19490ad9961d33c05e76bacb5
sanpi0205 <sanpi0205@gmail.com>
2016-01-19 11:06:24 +0800

    add knn algorithm

--- knn/knn.py
+++ knn/knn.py
@@ -1,15 +1,33 @@
 # coding: UTF-8
 
-from numpy import *
+import numpy as np
 import operator
 
 def createDataSet():
-	group = array([[1.0,1.1], [1.0,1.0], [0,0], [0,0.1]])
-	labels = ['A', 'A', 'B', 'B']
-	return group, labels
+    """创建模拟数据"""
+    
+    group = np.array([[1.0,1.1], [1.0,1.0], [0,0], [0,0.1]])
+    labels = np.array(['A', 'A', 'B', 'B'])
+    return group, labels
 
 def classify0(inX, dataSet, labels, k):
-	# sss
+    """利用knn进行分类"""
+    # 获取数据大小
+    dataSize, dim = dataSet.shape
+    # 将新数据扩展为与样本一样大小的矩阵，利用矩阵进行计算
+    diffMat = np.tile(inX, (dataSize, 1)) - dataSet
+    # 将矩阵元素平方
+    squareMat = diffMat ** 2
+    # 计算距离，沿轴=1求和
+    distances = squareMat.sum(axis=1) ** 0.5
+    # 计算排序大小，返回下标，利用该下标获取标签，并分类
+    sortedDistances = distances.argsort()
+    # 定义
+    classCount = {}
+    
+    
+    
+    
 	dataSetSize = dataSet.shape[0]
 	diffMat = tile(inX, (dataSetSize, 1)) - dataSet
 	sqDiffMat = diffMat ** 2
