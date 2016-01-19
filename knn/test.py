28b58ae298cf4a3a6cbfaf0ea3591d6160278fc9
sanpi0205 <sanpi0205@gmail.com>
2016-01-19 16:44:55 +0800

    测试knn方法

--- knn/test.py
+++ knn/test.py
@@ -1,8 +1,12 @@
 # coding: UTF-8
 
-import knn
+
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+
+import knn
+import mnist_test
 
 group, labels = knn.createDataSet()
 bb = knn.classify0([0,0], group, labels, 3)
@@ -20,5 +24,9 @@ plt.show()
 # 测试误判率
 reload(knn)
 # testRatio为测试集比例，k为邻居个数
-knn.knnTest('data/datingTestSet2.txt',testRatio=0.2, k=3)
+knn.knnTest('../data/datingTestSet2.txt',testRatio=0.2, k=3)
 
+
+# 测试手写数字识别
+
+mnist_test.mnist_test(500,100, k=7)
