82afc09145b60b08bda31634779adbcaa3a07d21
sanpi0205 <sanpi0205@gmail.com>
2016-01-19 11:17:24 +0800

    add knn algorithm

--- knn/knn.py
+++ knn/knn.py
@@ -11,7 +11,20 @@ def createDataSet():
     return group, labels
 
 def classify0(inX, dataSet, labels, k):
-    """利用knn进行分类"""
+    """利用knn进行分类
+    
+    参数
+    --------
+    inX : 待样本点
+    
+    dataSet: 样本数据
+    
+    labels: 样本类别（样本标签）
+    
+    k: 邻居个数，knn算法参数
+    
+    
+    """
     # 获取数据大小
     dataSize, dim = dataSet.shape
     # 将新数据扩展为与样本一样大小的矩阵，利用矩阵进行计算
@@ -22,22 +35,14 @@ def classify0(inX, dataSet, labels, k):
     distances = squareMat.sum(axis=1) ** 0.5
     # 计算排序大小，返回下标，利用该下标获取标签，并分类
     sortedDistances = distances.argsort()
-    # 定义
+    # 定义类别计数器，统计k个最近邻的标签
     classCount = {}
+    for i in xrange(k):
+        label  = labels[sortedDistances[i]]
+        classCount[label] = classCount.get(label, 0) + 1
     
+    # 对类标签排序,并按照降序排列
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse = True)
     
-    
-    
-	dataSetSize = dataSet.shape[0]
-	diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-	sqDiffMat = diffMat ** 2
-	sqDistances = sqDiffMat.sum(axis=1)
-	distances = sqDistances**0.5
-	sortedDistIndicies = distances.argsort()
-	classCount = {}
-	for i in range(k):
-		voteIlabel = labels[sortedDistIndicies[i]]
-		classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-	sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), 
-			reverse=True)
-	return sortedClassCount[0][0] 
+    # 返回排序后第一个类别
+    return sortedClassCount[0][0]
