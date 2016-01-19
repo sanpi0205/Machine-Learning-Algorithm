89b1238ce13b272b03c72914ec646e72831f0c19
sanpi0205 <sanpi0205@gmail.com>
2016-01-19 13:26:02 +0800

    add new knn algorithm

--- knn/knn.py
+++ knn/knn.py
@@ -71,6 +71,52 @@ def knn2(inX, dataSet, labels, k):
     
     # 返回排序后第一个类别
     return sortedClassCount[0][0]
+
+
+def file2matrix(filename):
+    """读取数据文件"""
     
+    fileRead = open(filename)
+    
+    dataSet = []
+    labels = []
+    
+    # 按行读入文件, 按行读取数据，每次读入一行
+    for line in fileRead:
+        line = line.strip()
+        line = line.split('\t')
+        # 将文本数据转换浮点数据
+        data = line[0:3]
+        data = map(float, data)
+        # 将文本分类数据转换为整数
+        label = line[-1]
+        label = int(label)
         
+        dataSet.append(data)
+        labels.append(label)
+    
+    dataSet = np.array(dataSet)
+    labels = np.array(labels)
+    return dataSet, labels    
+    
+    
+def autoNorm(dataSet):
+    """数据归一化处理
+    归一化处理公示很多，此处用 (x - min) / (max -min)
+    也可以用 (x - mean) / sd
+    """
+
+    minValues = dataSet.min(0)
+    maxValues = dataSet.max(0)
+    dataRange = maxValues - minValues
+    # 利用矩阵计算，用空间换时间
+    dataSize,dim = dataSet.shape
+    normData = np.zeros((dataSize, dim))
+    normData = dataSet - np.tile(minValues, (dataSize, 1))
+    normData = normData / np.tile(dataRange, (dataSize, 1))
+    
+    return 
+    
+    
+
         
--- knn/test.py
+++ knn/test.py
@@ -1,7 +1,18 @@
 # coding: UTF-8
 
 import knn
+import matplotlib
+import matplotlib.pyplot as plt
 
 group, labels = knn.createDataSet()
 bb = knn.classify0([0,0], group, labels, 3)
 print bb
+cc = knn.knn2([0,0], group, labels, 3)
+
+# 可视化数据
+dataSet, labels = knn.file2matrix('data/datingTestSet2.txt')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(dataSet[:,0], dataSet[:,1], s=15.0*labels, c=15.0*labels)
+plt.show()
+
