4bac4d0619dba3704639dd76a438d0a43d4b72d9
sanpi0205 <sanpi0205@gmail.com>
2016-02-01 16:56:54 +0800

    随机梯度与随机梯度修改

--- logistic/logisticAgorithm.py
+++ logistic/logisticAgorithm.py
@@ -7,6 +7,7 @@ Created on Mon Feb  1 11:12:37 2016
 
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 def loadData():
     dataSet = []
@@ -37,14 +38,17 @@ def cost(y, yhat):
     costValue = -(1.0/n) * np.sum( y*np.log(yhat) + (1-y)*np.log(1-yhat) )
     return costValue
 
-def costDerivative(y, yhat):
-    """"""
-    return (yhat - y)
+def costDerivative(dataSet, y, yhat):
+    """严格按照函数定义，损失函数的导数应该是 grad  = (1/n) * x^T * (yhat - y)
+    """
+    n = dataSet.shape[0]
+    grad = np.dot(dataSet.transpose(), yhat-y) / n
+    return grad
 
 def gradient(dataSet, y):
     
     n, p = np.shape(dataSet)
-    alpha = 0.003
+    alpha = 0.3
     maxCycles = 300
     # 初始化权重，也可以是随机初始化    
     weights = np.ones((p,1))
@@ -55,8 +59,8 @@ def gradient(dataSet, y):
     for i in xrange(maxCycles):
         yhat = sigmoid( np.dot(dataSet, weights))
         costValues.append(cost(y, yhat))
-        grad = costDerivative(y, yhat)
-        weights += -alpha * np.dot(dataSet.transpose(), grad)
+        grad = costDerivative(dataSet, y, yhat)
+        weights += -alpha *  grad
         weightsSum.append(np.sum( np.abs(weights)))
         
     return costValues, weightsSum, weights
@@ -83,3 +87,40 @@ def plotFitLine(weights):
     ax.plot(x, y)
     plt.show()
     
+def SGD(dataSet, y):
+    """随机梯度算法与梯度下降算法的不同在于，随机梯度算法每次只利用一个样本求解梯度，
+    之后将该梯度用于迭代，而梯度下降算法每次迭代需要利用所有数据，算法的计算开销比较大，
+    特别是当数据量比较大时，随机梯度算法效率更高。
+    当然随机梯度算法，也可以每次利用有限样本求接梯度，成为 mini_batch 的随机梯度算法，
+    算法具体代码参加：[Neural Network and Deep Learning]
+    """
+    n, p = dataSet.shape
+    alpha = 0.01
+    weights = np.ones(p)
+    for i in xrange(n):
+        h = sigmoid(np.sum(dataSet[i] * weights))
+        error = h - y[i]
+        weights += -alpha * dataSet[i] * error
+    return weights
+
+def SGDmodify(dataSet, y, numIter = 150):
+    
+    n, p = np.shape(dataSet)
+    weights = np.ones(p)
+    
+    for j in xrange(numIter):
+        dataIndex = range(n)
+        for i in xrange(n):
+            alpha = 4 / (1.0 + i + j) + 0.01
+            randomNum = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(np.sum(dataSet[dataIndex[randomNum]] * weights))
+            error = h - y[dataIndex[randomNum]]
+            weights += -alpha * dataSet[dataIndex[randomNum]] * error
+            del(dataIndex[randomNum])
+    return weights
+    
+    
+    
+    
+
+        
